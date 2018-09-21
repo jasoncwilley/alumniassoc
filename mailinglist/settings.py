0534ff992c98942ee6b4473ccc9534e86b896161
jasoncwilley <jason.c.willey@gmail.com>
2018-09-21 15:41:11 -0400

    initial

--- mailinglist/settings.py
+++ mailinglist/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'signup',
     'widget_tweaks',
     'bootstrap3',
+    'whitenoise'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mailinglist.urls'
