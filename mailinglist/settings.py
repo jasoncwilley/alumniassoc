8fde06a1a9d9af3690041c7b8a03f13b5cc7904e
jasoncwilley <jason.c.willey@gmail.com>
2018-09-21 15:30:44 -0400

    initial

--- mailinglist/settings.py
+++ mailinglist/settings.py
@@ -125,4 +125,4 @@ USE_L10N = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
