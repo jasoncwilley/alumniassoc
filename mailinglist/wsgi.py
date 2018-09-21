58ab6b3b77bf3083597a18df4e87a59cfdd9ddc2
jasoncwilley <jason.c.willey@gmail.com>
2018-09-21 16:54:27 -0400

    initial

--- mailinglist/wsgi.py
+++ mailinglist/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from mailinglist import MyWSGIApp
 
+application = MyWSGIApp()
+application = WhiteNoise(mailinglist, root='static')
+application = add_files('/signup/static')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mailinglist.settings")
 
 application = get_wsgi_application()
